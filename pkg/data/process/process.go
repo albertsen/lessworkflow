b34a95ec337561190bcabb9f0dd6a6c67f99eb5d
Juergen Albertsen <mail@juergenalbertsen.de>
2019-01-14 22:18:51 +0100

    Cleared up processservice

---
+++ pkg/data/process/process.go
@@ -0,0 +1,13 @@
+package process
+
+import "time"
+
+type Process struct {
+	tableName     struct{}   `sql:"prcesses"`
+	ID            string     `json:"id"`
+	Status        string     `json:"status"`
+	TimeCreated   *time.Time `json:"timeCreated"`
+	TimeUpdated   *time.Time `json:"timeUpdated"`
+	DocumentURL   string     `json:"documentURL"`
+	ProcessDefURL string     `json:"processDefURL"`
+}
